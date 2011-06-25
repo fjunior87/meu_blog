6086af3dc1a498d3b7772fa26fbffb767c58e357
Junior Ribeiro <fjunior87@gmail.com>
2011-06-24 21:38:05 -0300

    Finalizado capitulo de arquivos estaticos

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from models import Artigo
+
 # Create your views here.
 def artigo(request, artigo_id):
 	artigo = Artigo.objects.get(id=artigo_id)
-	return render_to_response('blog/artigo.html',locals())
+	return render_to_response('blog/artigo.html',locals(),
+		context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
 	{'feed_dict': {'ultimos': UltimosArtigos}}),
 	url(r'^artigo/(?P<artigo_id>\d+)/$','blog.views.artigo'),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^media/(.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     # Examples:
     # url(r'^$', 'meu_blog.views.home', name='home'),
     # url(r'^meu_blog/', include('meu_blog.foo.urls')),
